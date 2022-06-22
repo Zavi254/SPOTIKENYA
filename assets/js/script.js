55e3fa119c737ee20aef76fdeb20fbbc7fa8a554
Steven-Otieno <steveotianga8991@gmail.com>
2022-06-23 00:24:12 +0300

    Stored the wicked title content in variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,10 @@ const toggleButton = document.querySelector('#toggleIcon');
 const toggleButtonDark = document.querySelector('#toggleIconOff');
 const heroSection = document.querySelector('.heroSectionWords');
 const albumPhotos = document.getElementsByClassName('albumPhotos');
-const wickedPhoto = albumPhotos[0];
-const theKnowing = albumPhotos[1]
-const twentyEight = albumPhotos[2]
-const header = document.querySelector('header')
+const albumPics = [albumPhotos[0], albumPhotos[1], albumPhotos[2]]
+const header = document.querySelector('header');
+const wickedTitle = document.querySelector('#wickedTitle');
+const  wickedContent = document.querySelector('#wickedContent');
 
 
 function parseResponse(response){
@@ -22,18 +22,9 @@ function returnedData(data){
         albumPhotos[1].setAttribute(`src`, `${data.mvids[3].strTrackThumb}`)
         albumPhotos[2].setAttribute(`src`, `${data.mvids[4].strTrackThumb}`)
     }
-    function coverPhotosHover(){
-        wickedPhoto.addEventListener('mouseover', () => {
-            
-        })
-        theKnowing.addEventListener('mouseover', () => {
+    function displayCoverPhotoDescription(){
 
-        })
-        twentyEight.addEventListener('mouseover', () => {
-
-        })
     }
-coverPhotosHover()
 displayImage()
     
     
@@ -44,18 +35,20 @@ displayImage()
 function toggleButtonOn(icon){
     icon.addEventListener('click', () => {
         heroSection.style.color = "#1DB954"
-        header.style.backgroundImage = "url('https://images.unsplash.com/photo-1487180144351-b8472da7d491?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80')"
+        header.style.backgroundImage = "url('https://images.unsplash.com/photo-1506157786151-b8491531f063?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')"
     })
 }
 function toggleButtonOff(icon){
     icon.addEventListener("click", () => {
         heroSection.style.color = "#fff"
-        header.style.backgroundImage = "url('https://images.unsplash.com/photo-1470225620780-dba8ba36b745?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')"
+        header.style.backgroundImage = "url('https://images.unsplash.com/photo-1576269601089-aad0bbfa4f11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')"
     })
 }
 toggleButtonOff(toggleButtonDark)
 toggleButtonOn(toggleButton);
 
+console.log(albumPics)
 
 })
+
 
