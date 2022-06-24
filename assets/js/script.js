9024b6cfc54996746abd32fa24d450092e0c21f9
Steven-Otieno <steveotianga8991@gmail.com>
2022-06-24 20:02:52 +0300

    removed some navigation links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const toggleButton = document.querySelector('#toggleIcon');
     const toggleButtonDark = document.querySelector('#toggleIconOff');
-    const heroSection = document.querySelector('.heroSectionWords');
     const albumPhotos = document.getElementsByClassName('albumPhotos');
     const header = document.querySelector('header');
     const wickedTitle = document.querySelector('#wickedTitle');
@@ -130,14 +129,6 @@ document.addEventListener("DOMContentLoaded", () => {
         })
     })
 
-
-
-
-    
-
-
-
-
     fetch('https://www.theaudiodb.com/api/v1/json/2/search.php?s=khalid')
     .then(response => response.json())
     .then(artistData)
@@ -232,21 +223,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function toggleButtonOn(icon){
         icon.addEventListener('click', () => {
-            heroSection.style.color = "#1DB954"
             header.style.backgroundImage = "url('https://images.unsplash.com/photo-1506157786151-b8491531f063?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')"
         })
     }
     function toggleButtonOff(icon){
         icon.addEventListener("click", () => {
-            heroSection.style.color = "#fff"
             header.style.backgroundImage = "url('https://images.unsplash.com/photo-1576269601089-aad0bbfa4f11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')"
         })
     }
 function parseResponse(response){
     return response.json()
 }
-
-
 
 function returnedData(data){
         albumPhotos[0].setAttribute(`src`, `${data.mvids[2].strTrackThumb}`)
@@ -278,17 +265,9 @@ function returnedData(data){
 
 displayCoverPhotoDescription()
 
-
-
 }
-
-
-
-
-
 toggleButtonOff(toggleButtonDark)
 toggleButtonOn(toggleButton);
-
 
 })
 
